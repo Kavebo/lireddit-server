f3fc352ee14c31faea2c8c1b4fb9d864c6ce4ec1
KavecanskyJ <jozef.kavecansky@gmail.com>
2020-10-04 23:00:26 +0200

    feat(Login/Logout): add functionality to login and logout

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import "reflect-metadata";
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import microConfig from "./mikro-orm.config";
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { buildSchema } from "type-graphql";
-import { HelloResolver } from "./resolvers/hello";
-import { PostResolver } from "./resolvers/post";
-import { UserResolver } from "./resolvers/user";
-import cors from "cors";
+import 'reflect-metadata';
 
-import redis from "redis";
-import session from "express-session";
-import connectRedis from "connect-redis";
+import { MikroORM } from '@mikro-orm/core';
+import { ApolloServer } from 'apollo-server-express';
+import connectRedis from 'connect-redis';
+import cors from 'cors';
+import express from 'express';
+import session from 'express-session';
+import redis from 'redis';
+import { buildSchema } from 'type-graphql';
+
+import { __prod__, COOKIE_NAME } from './constants';
+import microConfig from './mikro-orm.config';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
+import { UserResolver } from './resolvers/user';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -26,11 +27,10 @@ const main = async () => {
 
   app.use(
     session({
-      name: "qid",
+      name: COOKIE_NAME,
       store: new RedisStore({
         client: redisClient,
         disableTouch: true,
-        disableTTL: true,
       }),
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years,
